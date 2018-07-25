b9db89f4cdbea62d12d4b9d9955b3683a64d3e8a
Julia <jj.akateva@gmail.com>
2018-07-25 11:35:54 -0700

    js file

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function toInput(id) {
+    let value = document.getElementById(id).value;
+    document.getElementById('result').value += value;
+}
+
+function subtract(){
+    const a = document.getElementById('').value;
+    const b = document.getElementById('').value;
+    return a-b;
+}
+
+function clearValue() {
+    document.getElementById('result').value = '';
+}
+
